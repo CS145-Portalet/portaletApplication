cb55131b0d8f6d298ec5868e8559649edcc9f265
AltsManansala <jgmanansala1@up.edu.ph>
2025-05-21 04:33:42 +0800

    ui: light mode and appbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,24 +1,26 @@
 <script lang="ts">
 	import '../app.css';
-	import { Navigation } from '@skeletonlabs/skeleton-svelte';
+	import { AppBar } from '@skeletonlabs/skeleton-svelte';
 
-	import IconMenu from '@lucide/svelte/icons/menu';
-	import IconHome from '@lucide/svelte/icons/house';
-	import IconSettings from '@lucide/svelte/icons/settings';
-	import User from '@lucide/svelte/icons/circle-user';
-	import CircleUser from '@lucide/svelte/icons/circle-user';
+	import CirclePlus from '@lucide/svelte/icons/circle-plus';
 
 	let { children } = $props();
 
 </script>
 
-<div></div>
-<div>
-	<Navigation.Bar>
-		<Navigation.Tile label="Home" href="/"></Navigation.Tile>
-		<Navigation.Tile label="Add Device" href="/addDevice"></Navigation.Tile>
-	</Navigation.Bar>
-</div>
+<AppBar>
+	{#snippet lead()}
+		<a href="/" class="hidden sm:block">
+			logo 
+		</a>
+	{/snippet}
+
+	{#snippet trail()}
+		<a href="/addDevice">
+			<CirclePlus />
+		</a>
+	{/snippet}
+</AppBar>
 
 {@render children()}
 	
--- src/routes/addDevice/+page.svelte
+++ src/routes/addDevice/+page.svelte
@@ -47,51 +47,49 @@
 	}
 </script>
 
-<main class="mt-10 text-gray-100">
-	<div>
-		<!-- Input box -->
-		<div class="bg-secondary mx-auto flex max-w-4xl flex-col rounded-lg p-5">
-			<h1 class="text-center text-2xl text-white">Create New Device</h1>
 
-			<div class="my-4 flex flex-col">
-				<label for="event-name">Device Nickname</label>
-				<input
-					id="device-nickname"
-					bind:value={deviceNickname}
-					type="text"
-					placeholder="Enter a nickname for the device"
-					class="border-borderclr border bg-transparent py-4 pr-24 pl-5"
-				/>
-			</div>
-			<!-- Device City -->
-			<div class="my-4 flex flex-col">
-				<label for="event-name">City Location</label>
-				<input
-					id="device-city"
-					bind:value={deviceCity}
-					type="text"
-					placeholder="Enter the city where the device is located"
-					class="border-borderclr border bg-transparent py-4 pr-24 pl-5"
-				/>
-			</div>
-			<!--Device Street Address-->
-			<div class="my-4 flex flex-col">
-				<label for="event-name">Street Address</label>
-				<input
-					id="device-streetAddress"
-					bind:value={deviceStreetAddress}
-					type="text"
-					placeholder="Enter the street address where the device is located"
-					class="border-borderclr border bg-transparent py-4 pr-24 pl-5"
-				/>
-			</div>
 
-			<button
-				disabled={loading}
-				on:click={createDevice}
-				class="mt-8 bg-white px-8 py-2 text-black disabled:cursor-not-allowed disabled:bg-white/25"
-				>{loading ? 'Creating' : 'Create'}</button
-			>
-		</div>
+<div class="mx-auto flex max-w-4xl flex-col rounded-lg p-5">
+	<h1 class="text-center text-2xl">Create New Device</h1>
+
+	<div class="my-4 flex flex-col">
+		<label for="event-name">Device Nickname</label>
+		<input
+			id="device-nickname"
+			bind:value={deviceNickname}
+			type="text"
+			placeholder="Enter a nickname for the device"
+			class="border-borderclr border bg-transparent py-4 pr-24 pl-5"
+		/>
 	</div>
-</main>
+	<!-- Device City -->
+	<div class="my-4 flex flex-col">
+		<label for="event-name">City Location</label>
+		<input
+			id="device-city"
+			bind:value={deviceCity}
+			type="text"
+			placeholder="Enter the city where the device is located"
+			class="border-borderclr border bg-transparent py-4 pr-24 pl-5"
+		/>
+	</div>
+	<!--Device Street Address-->
+	<div class="my-4 flex flex-col">
+		<label for="event-name">Street Address</label>
+		<input
+			id="device-streetAddress"
+			bind:value={deviceStreetAddress}
+			type="text"
+			placeholder="Enter the street address where the device is located"
+			class="border-borderclr border bg-transparent py-4 pr-24 pl-5"
+		/>
+	</div>
+
+	<button
+		disabled={loading}
+		on:click={createDevice}
+		class="mt-8 bg-white px-8 py-2 text-black disabled:cursor-not-allowed disabled:bg-white/25"
+		>{loading ? 'Creating' : 'Create'}
+	</button>
+</div>
+
