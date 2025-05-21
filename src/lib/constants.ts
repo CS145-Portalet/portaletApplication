c42102d403b14f228d0dfe01c2afc3c086e4d298
AltsManansala <jgmanansala1@up.edu.ph>
2025-05-21 15:49:11 +0800

    feat: loading page

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,7 +1,7 @@
 export const statusMap: Record<number, string> = {
 	4: 'No Data',
-	0: 'Dry/No Water',
+	0: 'Dry',
 	1: 'Low',
 	2: 'Medium',
-	3: 'High Water Level'
+	3: 'High'
 };
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,15 +8,19 @@
 
 </script>
 
-<AppBar>
+<AppBar spaceY="align-middle" background="color-surface-50">
 	{#snippet lead()}
 		<a href="/" class="hidden sm:block">
 			logo 
 		</a>
+
 	{/snippet}
 
 	{#snippet trail()}
-		<a href="/addDevice">
+		<a href="/addDevice" type="button" class="btn btn-sm preset-filled-primary-500 hidden sm:flex" >
+			<CirclePlus /><p class="font-semibold">Add Device</p>
+		</a>
+		<a href="/addDevice" type="button" class="btn preset-filled-primary block sm:hidden">
 			<CirclePlus />
 		</a>
 	{/snippet}
