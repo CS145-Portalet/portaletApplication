f9100adc6f9d5ab2ef1dc295a2b2b038d0e54336
AltsManansala <jgmanansala1@up.edu.ph>
2025-05-23 03:49:06 +0800

    ui: edit and add device screens

--- src/routes/main/[deviceID]/editDevice/+page.svelte
+++ src/routes/main/[deviceID]/editDevice/+page.svelte
@@ -3,6 +3,8 @@
 	import { doc, updateDoc } from 'firebase/firestore';
 	import { db } from '$lib/firebase.js';
 
+	import Arrow from '@lucide/svelte/icons/arrow-left';
+
 	export let data;
 	console.log(data);
 	let pageTitle: string = data.device.nickname;
@@ -29,51 +31,69 @@
 
 		goto(`/main/${deviceID}`);
 	}
+
+	async function backHandler(){
+		goto(`/main/${deviceID}`);
+		return 0;
+	}
 </script>
+
+<div>
+	<a href={`/main/${deviceID}`} class="ml-3 flex">
+		<Arrow size={25} />
+	</a>
+</div>
+
 
 <main class="mt-10">
 	<div>
 		<!-- Input box -->
 		<div class="bg-secondary mx-auto flex max-w-4xl flex-col rounded-lg p-5">
-			<h1 class="text-center text-2xl text-white">Edit Device {pageTitle}</h1>
+			<h1 class="text-center text-2xl">Edit Device</h1>
 
 			<div class="my-4 flex flex-col">
-				<label for="event-name">Device Nickname</label>
+				<label for="event-name" class="ml-5">
+					Device Nickname
+				</label>
 				<input
 					id="device-nickname"
 					bind:value={deviceNickname}
 					type="text"
 					placeholder="Enter a nickname for the device"
-					class="border-borderclr border bg-transparent py-4 pr-24 pl-5"
+					class="border-0 rounded-full bg-primary-50 text-primary-700 py-4 pr-24 pl-5"
 				/>
 			</div>
 			<!-- Device City -->
 			<div class="my-4 flex flex-col">
-				<label for="event-name">City Location</label>
+				<label for="event-name" class="ml-5">
+					City Location
+				</label>
 				<input
 					id="device-city"
 					bind:value={deviceCity}
 					type="text"
 					placeholder="Enter the city where the device is located"
-					class="border-borderclr border bg-transparent py-4 pr-24 pl-5"
+					class="border-0 rounded-full bg-primary-50 text-primary-700 py-4 pr-24 pl-5"
 				/>
 			</div>
 			<!--Device Street Address-->
 			<div class="my-4 flex flex-col">
-				<label for="event-name">Street Address</label>
+				<label for="event-name" class="ml-5">
+					Street Address
+				</label>
 				<input
 					id="device-streetAddress"
 					bind:value={deviceStreetAddress}
 					type="text"
 					placeholder="Enter the street address where the device is located"
-					class="border-borderclr border bg-transparent py-4 pr-24 pl-5"
+					class="border-0 rounded-full bg-primary-50 text-primary-700 py-4 pr-24 pl-5"
 				/>
 			</div>
 
 			<button
 				disabled={loading}
 				on:click={editDevice}
-				class="mt-8 bg-white px-8 py-2 text-black disabled:cursor-not-allowed disabled:bg-white/25"
+				class="mt-8 btn preset-filled-primary-500 px-8 py-2 disabled:cursor-not-allowed disabled:bg-gray/25"
 				>{loading ? 'Editing' : 'Edit'}</button
 			>
 		</div>
