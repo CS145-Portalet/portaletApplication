d42bdcca6f6106d1c768614c65e95148f069241c
Koniiro <81352867+Koniiro@users.noreply.github.com>
2025-05-22 17:34:18 +0800

    ui: Added log in feedback

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,22 @@
 <script>
+	import { onMount } from 'svelte';
+	import { auth } from '$lib/firebase.js'
+	import { authStore } from '../store/store.js';
+
 	import Authenticate from '../lib/components/auth.svelte';
+
+	onMount(() => {
+		const unsubscribe = auth.onAuthStateChanged((user) => {
+			authStore.set({ user: user ?? null }); // Shorter and same logic
+		});
+		return unsubscribe;
+	});
 </script>
 
-<Authenticate />
+{#if $authStore.user}
+  <p>Loading Device Menu</p>
+  <p>Welcome to</p>
+  <p>Cubimon</p>
+{:else}
+  <Authenticate />
+{/if}
