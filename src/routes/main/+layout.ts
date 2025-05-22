d95ecf162f8b4588595d808878b898da5c7d12c0
Koniiro <81352867+Koniiro@users.noreply.github.com>
2025-05-22 13:27:57 +0800

    feat:un logged in user redirection

---
+++ src/routes/main/+layout.ts
@@ -0,0 +1,14 @@
+//import type { LayoutLoad } from './$types.js';
+import { redirect } from '@sveltejs/kit';
+import { get } from 'svelte/store';
+import { authStore } from '../../store/store.js'; // adjust path as needed
+
+export const load = async () => {
+	const user = get(authStore).user;
+
+	if (!user) {
+		throw redirect(302, '/');
+	}
+
+	return { user };
+};
